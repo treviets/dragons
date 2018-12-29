cb4a80ba35bb8ed54afab7b5e9d9e0d0bf086b5a
Nhan Nguyen Vu <vunhankhtn@gmail.com>
2018-12-29 10:44:34 +0700

    Add encryption

---
+++ dragons-application/src/main/java/net/dragons/service/EncryptionService.java
@@ -0,0 +1,8 @@
+package net.dragons.service;
+
+
+public interface EncryptionService {
+
+	String decrypt(String password);
+	
+}
---
+++ dragons-application/src/main/java/net/dragons/service/impl/EncryptionServiceImpl.java
@@ -0,0 +1,14 @@
+package net.dragons.service.impl;
+
+import net.dragons.service.EncryptionService;
+
+public class EncryptionServiceImpl implements EncryptionService {
+
+	@Override
+	public String decrypt(String password) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+}
