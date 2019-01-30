dbb2341d55893cdba056f8d3bb5dd39078789dbf
Nhan Nguyen Vu <vunhankhtn@gmail.com>
2019-01-30 21:43:15 +0700

    Add guest profile

---
+++ dragons-entity/src/main/java/net/dragons/jpa/entity/GuestProfile.java
@@ -0,0 +1,108 @@
+/**
+ * 
+ */
+package net.dragons.jpa.entity;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+/**
+ * @author NhanNguyen
+ *
+ */
+@Entity
+@Table(name="guest_profile")
+@NamedQuery(name="GuestProfile.findAll", query="SELECT r FROM GuestProfile r")
+public class GuestProfile {
+	
+	@JsonProperty("Id")
+	@Id
+	@Column(name="id")
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(name="customer_id")
+	private Long customerId;
+
+	@Column(name="first_name")
+	private String firstName;
+	
+	@Column(name="last_name")
+	private String lastName;
+	
+	@Column(name="passport_number")
+	private String passportNumber;
+	
+	@Column(name="nationality")
+	private String nationality;
+	
+	private Date expires;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+	
+	public Long getCustomerId() {
+		return customerId;
+	}
+
+	public void setCustomerId(Long customerId) {
+		this.customerId = customerId;
+	}
+	
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getPassportNumber() {
+		return passportNumber;
+	}
+
+	public void setPassportNumber(String passportNumber) {
+		this.passportNumber = passportNumber;
+	}
+
+	public Date getExpires() {
+		return expires;
+	}
+
+	public void setExpires(Date expires) {
+		this.expires = expires;
+	}
+
+	public String getNationality() {
+		return nationality;
+	}
+
+	public void setNationality(String nationality) {
+		this.nationality = nationality;
+	}
+
+
+	
+}
--- dragons-entity/src/main/java/net/dragons/jpa/entity/ShippingAddress.java
+++ dragons-entity/src/main/java/net/dragons/jpa/entity/ShippingAddress.java
@@ -49,6 +49,68 @@ public class ShippingAddress {
 	@Column(name = "postal_code")
 	private String postalCode;
 
-	
-	
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Long getCustomerId() {
+		return customerId;
+	}
+
+	public void setCustomerId(Long customerId) {
+		this.customerId = customerId;
+	}
+
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+	public String getPrimaryAddress() {
+		return primaryAddress;
+	}
+
+	public void setPrimaryAddress(String primaryAddress) {
+		this.primaryAddress = primaryAddress;
+	}
+
+	public String getOptionalAddress() {
+		return optionalAddress;
+	}
+
+	public void setOptionalAddress(String optionalAddress) {
+		this.optionalAddress = optionalAddress;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getProvince() {
+		return province;
+	}
+
+	public void setProvince(String province) {
+		this.province = province;
+	}
+
+	public String getPostalCode() {
+		return postalCode;
+	}
+
+	public void setPostalCode(String postalCode) {
+		this.postalCode = postalCode;
+	}
+
 }
