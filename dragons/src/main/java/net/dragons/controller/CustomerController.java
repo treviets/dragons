eed1e52b17deb89718d4c71862ba1690b6b812df
Nguyen Vu Nhan <vunhankhtn@gmail.com>
2019-02-28 21:54:17 +0700

    update common

--- dragons/src/main/java/net/dragons/controller/CustomerController.java
+++ dragons/src/main/java/net/dragons/controller/CustomerController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import net.dragons.commom.util.Utils;
+import net.dragons.common.util.Utils;
 import net.dragons.dto.CustomerDetailDto;
 import net.dragons.dto.CustomerDto;
 import net.dragons.dto.ResponseDto;
