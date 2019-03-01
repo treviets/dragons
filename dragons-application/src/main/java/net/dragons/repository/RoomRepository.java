2fe6407853717adf9a19e7b9c9c07e1be9b7fcd7
truongnguyen111995 <nguyennhattruong111995@gmail.com>
2019-03-01 09:37:20 +0700

    fix sql

--- dragons-application/src/main/java/net/dragons/repository/RoomRepository.java
+++ dragons-application/src/main/java/net/dragons/repository/RoomRepository.java
@@ -25,7 +25,7 @@ public interface RoomRepository extends JpaRepository<Room, Integer>{
 	List<Room> findByHomeId(Long homeId);
 	
 	@Query(value = "SELECT room FROM Room room WHERE (id NOT IN (:ids) OR :ids is NULL) "
-			+ "AND (homeId =:homeId OR :homeId is NULL) "
+			+ "AND (homeId =:homeId OR :homeId is NULL OR :homeId = 0) "
 			+ "AND (numberOfGuest >=:numberOfGuest OR :numberOfGuest is NULL OR :numberOfGuest = 0) "
 			+ "AND ((price >= :min OR :min is NULL)  AND (price <= :max OR :max is NULL)) "
 			+ "AND (roomType = :roomtype OR :roomtype = 0)")
