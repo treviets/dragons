90f26b86028da3ce029fac298e2dbb32eabfd7d5
Nguyen Vu Nhan <vunhankhtn@gmail.com>
2019-03-03 14:00:12 +0700

    Update get room

--- dragons-application/src/main/java/net/dragons/service/impl/RoomServiceImpl.java
+++ dragons-application/src/main/java/net/dragons/service/impl/RoomServiceImpl.java
@@ -1,10 +1,8 @@
 package net.dragons.service.impl;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
-import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import javax.persistence.EntityManager;
@@ -12,7 +10,6 @@ import javax.persistence.EntityManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import net.dragons.jpa.entity.BnbBooking;
 import net.dragons.jpa.entity.Room;
 import net.dragons.repository.RoomRepository;
 import net.dragons.service.BnbBookingService;
@@ -62,6 +59,7 @@ public class RoomServiceImpl implements RoomService {
 		if (result.size() == 0) {
 			result.add(Long.valueOf(0));
 		}
+		
 		// Get List of Room not In booked list
 		List<Room> rooms = roomRepository.findRoomsNotBooked(roomtype, min, max, result, homeId, numberOfGuest);
 		
@@ -73,12 +71,6 @@ public class RoomServiceImpl implements RoomService {
 	public List<Room> getByHomeId(Long homeId) {
 		return roomRepository.findByHomeId(homeId);
 	}
-
-//	@Override
-//	public List<Room> getRoomsNotBook(long[] ids, Long homeId, Integer numberOfGuest, String min, String max, Integer roomtype) {
-//		return roomRepository.findRoomsNotBooked(roomtype, min, max, ids, homeId, numberOfGuest);
-//	}
-//	
 
 	@Override
 	public List<Room> getRoomsNotBook(List<Long> ids, Long homeId, Integer numberOfGuest, String min, String max, Integer roomtype) {
